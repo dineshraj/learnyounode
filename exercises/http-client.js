9231f48407c489c3d2d2d18adc8f15f1abd5431f
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2016-03-11 17:15:28 +0000

    exercise 8

---
+++ exercises/http-client.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+http.get(process.argv[2], function (response) {
+
+    response
+        .setEncoding('utf8')
+        .on('data', function (data) {
+            console.log(data);
+        })
+        .on('error', function (data) {
+            console.error(data);
+        });
+});
